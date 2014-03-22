a4d7aebd7eab726d8b501c2681cbae3dc0f1a3fb
HIMBERT Guillaume <guillaume.himbert@gmail.com>
2014-03-22 10:28:57 +0100

    minor evolutions

--- dlna/dlnarootfolder.cpp
+++ dlna/dlnarootfolder.cpp
@@ -1,13 +1,31 @@
 #include "dlnarootfolder.h"
 
 DlnaRootFolder::DlnaRootFolder(Logger* log, QString host, int port, QObject *parent):
-    DlnaStorageFolder(log, host, port, parent)
+    DlnaStorageFolder(log, host, port, parent),
+    children()
 {
     // For root node, id=0
     setId("0");
+}
 
-    // no need to parse children, child is added manually with function addFolder
-    setDiscovered(true);
+void DlnaRootFolder::addChild(DlnaResource *child) {
+
+    if (child == 0) {
+        log->ERROR(QString("Child is null, unable to append child to node %1").arg(getName()));
+    } else {
+        if (!child->getId().isNull()) {
+            if (child->getDlnaParent() != 0) {
+                log->ERROR(QString("Node %1 already has an ID %2, which is overridden now. The previous parent node was: %3").arg(child->getName()).arg(child->getResourceId()).arg(child->getDlnaParent()->getName()));
+            } else {
+                log->ERROR(QString("Node %1 already has an ID %2, which is overridden now.").arg(child->getName()).arg(child->getResourceId()));
+            }
+        }
+
+        child->setId(QString("%1").arg(children.length()+1));
+        children.append(child);
+        child->setDlnaParent(this);
+    }
+
 }
 
 bool DlnaRootFolder::addFolder(QString folder) {
--- dlna/dlnarootfolder.h
+++ dlna/dlnarootfolder.h
@@ -10,8 +10,6 @@ class DlnaRootFolder : public DlnaStorageFolder
 public:
     DlnaRootFolder(Logger* log, QString host, int port, QObject *parent = 0);
 
-    virtual bool discoverChildren() { return true; }
-
     // Any resource needs to represent the container or item with a String.
     // String to be showed in the UPNP client.
     virtual QString getName() const { return QString("root"); }
@@ -21,8 +19,21 @@ public:
     // Returns the DisplayName that is shown to the Renderer.
     virtual QString getDisplayName() { return getName(); }
 
+    /*
+     * Adds a new DLNAResource to the child list.
+     * Only useful if this object is of the container type.
+     */
+    void addChild(DlnaResource *child);
+    void clearChildren() { children.clear(); }
+
+    virtual int getChildrenSize() { return children.size(); }
+    virtual DlnaResource* getChild(int index, QObject *parent = 0) { Q_UNUSED(parent) return children.at(index); }
+
     // returns true if the folder is added to Root.
     virtual bool addFolder(QString folder);
+
+private:
+    QList<DlnaResource*> children;
 };
 
 #endif // DLNAROOTFOLDER_H
