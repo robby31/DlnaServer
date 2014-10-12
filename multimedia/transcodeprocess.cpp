c234a3e74b6d6de2488b1d199fc20c0e1cf8c181
HIMBERT Guillaume <guillaume.himbert@gmail.com>
2014-10-12 23:25:08 +0200

    update TranscodeProcess to manage position, pause and resume become private, class manage automatically its buffer size.

--- multimedia/transcodeprocess.cpp
+++ multimedia/transcodeprocess.cpp
@@ -5,13 +5,79 @@ const QString TranscodeProcess::CRLF = "\r\n";
 TranscodeProcess::TranscodeProcess(Logger *log, QObject *parent) :
     QProcess(parent),
     m_log(log != 0 ? log : new Logger(this)),
+    m_pos(0),
     transcodeClock(),
     killTranscodeProcess(false),
-    m_paused(false)
+    m_paused(false),
+    maxBufferSize(1024*1024*100)  // 100 MBytes
 {
+    connect(this, SIGNAL(readyReadStandardOutput()), this, SLOT(dataAvailable()));
     connect(this, SIGNAL(readyReadStandardError()), this, SLOT(appendTranscodingLogMessage()));
     connect(this, SIGNAL(error(QProcess::ProcessError)), this, SLOT(errorTrancodedData(QProcess::ProcessError)));
     connect(this, SIGNAL(finished(int)), this, SLOT(finishedTranscodeData(int)));
+}
+
+TranscodeProcess::~TranscodeProcess()
+{
+    if (state() == QProcess::Running)
+    {
+        kill();
+        waitForFinished(1000);
+    }
+}
+
+void TranscodeProcess::dataAvailable()
+{
+    if (m_log->isLevel(DEBG))
+        appendLog(QString("%1: received %2 bytes transcoding data."+CRLF).arg(QDateTime::currentDateTime().toString("dd MMM yyyy hh:mm:ss,zzz")).arg(bytesAvailable()));
+
+    // manage buffer
+    if (state() == QProcess::Running) {
+        if (bytesAvailable() > maxBufferSize && !m_paused)
+        {
+            // pause transcoding process
+            if (pause() == false)
+                m_log->Error(QString("Unable to pause transcoding: pid=%1").arg(pid()));
+
+            if (m_log->isLevel(DEBG))
+                appendLog(QString("%1: PAUSE TRANSCODING").arg(QDateTime::currentDateTime().toString("dd MMM yyyy hh:mm:ss,zzz")));
+        }
+    }
+}
+
+bool TranscodeProcess::atEnd() const
+{
+//    if (m_range && m_range->getEndByte()!=-1)
+//        return pos() > m_range->getEndByte();
+
+    if (state() == QProcess::Running)
+        return false;
+    else
+        return QProcess::atEnd();
+}
+
+qint64 TranscodeProcess::readData(char *data, qint64 maxlen)
+{
+    //    if (m_range && m_range->getEndByte()>0)
+    //    {
+    //        int bytesToRead = m_range->getEndByte() - pos() + 1;
+    //        if (bytesToRead>=0 && bytesToRead<maxSize)
+    //            return QFile::read(bytesToRead);
+    //    }
+
+    qint64 bytes = QProcess::readData(data, maxlen);
+    m_pos += bytes;
+
+    if (m_paused && state() != QProcess::NotRunning && bytesAvailable() < (maxBufferSize*0.5))
+    {
+        if (resume() == false)   // restart transcoding process
+            m_log->Error(QString("Unable to restart transcoding: pid=%1").arg(pid()));
+
+        if (m_log->isLevel(DEBG))
+            appendLog(QString("%1: RESUME TRANSCODING").arg(QDateTime::currentDateTime().toString("dd MMM yyyy hh:mm:ss,zzz")));
+    }
+
+    return bytes;
 }
 
 void TranscodeProcess::appendTranscodingLogMessage() {
@@ -27,12 +93,36 @@ void TranscodeProcess::errorTrancodedData(const ProcessError &error) {
     if (killTranscodeProcess == false) {
         // an error occured
         appendLog(QString("%2: ERROR Transcoding: %1."+CRLF).arg(errorString()).arg(QDateTime::currentDateTime().toString("dd MMM yyyy hh:mm:ss,zzz")));
+        setErrorString(errorString());
+        emit errorRaised(errorString());
     }
 }
 
 void TranscodeProcess::finishedTranscodeData(const int &exitCode) {
     appendLog(QString("%2: TRANSCODE FINISHED with exitCode %1."+CRLF).arg(exitCode).arg(QDateTime::currentDateTime().toString("dd MMM yyyy hh:mm:ss,zzz")));
     appendLog(QString("%2: TRANSCODING DONE in %1 ms."+CRLF).arg(QTime(0, 0).addMSecs(transcodeClock.elapsed()).toString("hh:mm:ss")).arg(QDateTime::currentDateTime().toString("dd MMM yyyy hh:mm:ss,zzz")));
+
+    if (m_log->isLevel(DEBG))
+        appendLog(QString("%1: finished transcoding, %2 remaining bytes."+CRLF).arg(QDateTime::currentDateTime().toString("dd MMM yyyy hh:mm:ss,zzz")).arg(bytesAvailable()));
+
+    if (isKilled() == false)
+    {
+        if (exitCode != 0)
+        {
+            // trancoding failed
+            setErrorString(QString("Transcoding finished with status %1").arg(exitCode));
+            emit errorRaised(QString("Transcoding finished with status %1").arg(exitCode));
+            emit status("Transcoding failed.");
+        }
+        else
+        {
+            emit status("Transcoding finished.");
+        }
+    }
+    else
+    {
+        emit status("Transcoding aborted.");
+    }
 }
 
 void TranscodeProcess::launch() {
--- multimedia/transcodeprocess.h
+++ multimedia/transcodeprocess.h
@@ -18,26 +18,39 @@ class TranscodeProcess : public QProcess
 
 public:
     explicit TranscodeProcess(Logger* log, QObject *parent = 0);
+    virtual ~TranscodeProcess();
 
     virtual void launch();
-    bool pause();
-    bool resume();
 
     bool isKilled() const { return killTranscodeProcess; }
     void killProcess();
 
     virtual int transcodeExitCode() const { return exitCode(); }
+    void setBufferSize(const qint64 &size) { if (size>0) maxBufferSize = size; }
+
+    virtual bool atEnd() const;
+
+    virtual qint64 pos() const { return m_pos; }
+    virtual qint64 readData(char *data, qint64 maxlen);
+
+
+private:
+    bool pause();
+    bool resume();
 
 signals:
-    void receivedTranscodingLogMessage(const QString &msg);
+    void LogMessage(const QString &msg);
+    void status(const QString &status);
+    void errorRaised(const QString &errorString);
 
-public slots:
+private slots:
+    void dataAvailable();
     void errorTrancodedData(const QProcess::ProcessError &error);
     void appendTranscodingLogMessage();
     void finishedTranscodeData(const int &exitCode);
 
 protected:
-    void appendLog(const QString &msg) { emit receivedTranscodingLogMessage(msg+CRLF); }
+    void appendLog(const QString &msg) { emit LogMessage(msg+CRLF); }
 
     Logger *m_log;
 
@@ -45,9 +58,12 @@ private:
     // Carriage return and line feed.
     static const QString CRLF;
 
+    qint64 m_pos;
+
     QElapsedTimer transcodeClock;
     bool killTranscodeProcess;  // true if the application aborts the transcoding
-    bool m_paused;   // true if the transcoding has been paused
+    bool m_paused;              // true if the transcoding has been paused
+    qint64 maxBufferSize;
 };
 
 #endif // TRANSCODEPROCESS_H
