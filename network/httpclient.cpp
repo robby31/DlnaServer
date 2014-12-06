17eb0417a837954709d5b575bd8defe046dc08f9
HIMBERT Guillaume <guillaume.himbert@gmail.com>
2014-12-06 22:30:52 +0100

    new class HttpClient

--- network/httpclient.cpp
+++ network/httpclient.cpp
@@ -1,6 +1,76 @@
 #include "httpclient.h"
 
-HttpClient::HttpClient(QObject *parent) :
-    QTcpSocket(parent)
+const QString HttpClient::CRLF = "\r\n";
+
+HttpClient::HttpClient(Logger *log, QObject *parent) :
+    QTcpSocket(parent),
+    m_log(log),
+    m_http10(true),
+    m_connectionType()
 {
+    if (!m_log)
+        qWarning() << "log is not available for" << this;
+
+    connect(this, SIGNAL(error(QAbstractSocket::SocketError)), this, SLOT(clientError(QAbstractSocket::SocketError)));
+    connect(this, SIGNAL(bytesWritten(qint64)), this, SLOT(_bytesWritten(qint64)));
+}
+
+HttpClient::~HttpClient()
+{
+}
+
+void HttpClient::closeClient()
+{
+    if (isHttp10() or connectionType() == "close")
+    {
+        if (socketDescriptor() == -1)
+        {
+            appendLogSignal(QString("%1: client already disconnected."+CRLF).arg(QDateTime::currentDateTime().toString("dd MMM yyyy hh:mm:ss,zzz")));
+        }
+        else
+        {
+            disconnectFromHost();
+            appendLogSignal(QString("%2: Close client (%1)"+CRLF).arg(socketDescriptor()).arg(QDateTime::currentDateTime().toString("dd MMM yyyy hh:mm:ss,zzz")));
+        }
+    }
+}
+
+void HttpClient::sendTextLine(const QString &msg)
+{
+    QByteArray tmp;
+
+    tmp.append(msg.toUtf8()).append(CRLF.toUtf8());
+
+    if (write(tmp) == -1)
+        logError("HTTP Request: Unable to send line: " + msg);
+
+    appendAnswerSignal(msg+CRLF);
+
+    logDebug("Wrote on socket: " + msg);
+}
+
+void HttpClient::sendHeader(const QString &httpType, const QHash<QString, QString> &header)
+{
+    logDebug("Send header.");
+    appendLogSignal(QString("%1: Send header."+CRLF).arg(QDateTime::currentDateTime().toString("dd MMM yyyy hh:mm:ss,zzz")));
+
+    sendTextLine(httpType.toUtf8());
+
+    foreach (const QString &param, header.keys())
+        sendTextLine(QString("%1: %2").arg(param).arg(header[param]).toUtf8());
+
+    sendTextLine("");
+
+//    flush();
+}
+
+void HttpClient::sendData(const QByteArray &data)
+{
+    // send the content
+    if (write(data) == -1)
+        logError("HTTP request: Unable to send content.");
+    else if (isLogLevel(DEBG))
+        appendLogSignal(QString("%1: bytes written?%2"+CRLF).arg(QDateTime::currentDateTime().toString("dd MMM yyyy hh:mm:ss,zzz")).arg(data.size()));
+
+//    flush();
 }
--- network/httpclient.h
+++ network/httpclient.h
@@ -2,17 +2,55 @@
 #define HTTPCLIENT_H
 
 #include <QTcpSocket>
+#include <QDateTime>
+
+#include "logger.h"
 
 class HttpClient : public QTcpSocket
 {
     Q_OBJECT
 public:
-    explicit HttpClient(QObject *parent = 0);
+    explicit HttpClient(Logger *log, QObject *parent = 0);
+    virtual ~HttpClient();
+
+    bool isHttp10() const { return m_http10; }
+
+    QString connectionType() const { return m_connectionType; }
+
+private:
+    bool isLogLevel(const LogLevel &level) const { return m_log ? m_log->isLevel(level) : false; }
+    void logError(const QString &message)  const { if (m_log) m_log->Error(message); }
+    void logDebug(const QString &message)  const { if (m_log) m_log->Debug(message); }
+    void logInfo(const QString &message)   const { if (m_log) m_log->Info(message); }
+    void logTrace(const QString &message)  const { if (m_log) m_log->Trace(message); }
 
 signals:
+    void appendLogSignal(const QString &msg);
+    void appendAnswerSignal(const QString &msg);
+
+    void bytesSent(const qint64 &size, const qint64 &bytesToWrite);
 
 public slots:
+    void closeClient();
 
+    void setHttp10(const bool &http10) { m_http10 = http10; }
+    void setConnectionType(const QString &connection) { m_connectionType = connection.toLower(); }
+
+    void sendTextLine(const QString &msg);
+    void sendHeader(const QString &httpType, const QHash<QString, QString> &header);
+    void sendData(const QByteArray &data);
+
+    void _bytesWritten(const qint64 &size) { emit bytesSent(size, bytesToWrite()); }
+    void clientError(QAbstractSocket::SocketError error) { emit appendLogSignal(QString("%2: Network Error: %1, %3"+CRLF).arg(error).arg(QDateTime::currentDateTime().toString("dd MMM yyyy hh:mm:ss,zzz")).arg(errorString())); }
+
+private:
+    // Carriage return and line feed.
+    static const QString CRLF;
+
+    Logger *m_log;
+
+    bool m_http10;
+    QString m_connectionType;
 };
 
 #endif // HTTPCLIENT_H
