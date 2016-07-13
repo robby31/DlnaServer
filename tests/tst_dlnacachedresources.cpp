ac15973ee52197647f023e36f8241f87d3e9a3b8
HIMBERT Guillaume <guillaume.himbert@gmail.com>
2016-07-13 22:47:39 +0200

    update tests

--- tests/tst_dlnacachedresources.cpp
+++ tests/tst_dlnacachedresources.cpp
@@ -27,7 +27,7 @@ void tst_dlnacachedresources::testCase_Library_NbMedias()
         if (query.last())
             nbMedias = query.at() + 1;
     }
-    QVERIFY2(nbMedias == 15283, QString("%1").arg(nbMedias).toUtf8().constData());
+    QVERIFY2(nbMedias == 15300, QString("%1").arg(nbMedias).toUtf8().constData());
     db.close();
 }
 
@@ -57,7 +57,7 @@ void tst_dlnacachedresources::testCase_Library_NbVideos()
         if (query.last())
             nbVideos = query.at() + 1;
     }
-    QVERIFY2(nbVideos == 1543, QString("%1").arg(nbVideos).toUtf8().constData());
+    QVERIFY2(nbVideos == 1560, QString("%1").arg(nbVideos).toUtf8().constData());
     db.close();
 }
 
