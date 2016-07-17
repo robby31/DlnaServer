4b68b65fdba39d3241496fd5c2bf6450a4699afd
HIMBERT Guillaume <guillaume.himbert@gmail.com>
2016-07-17 22:49:09 +0200

    new class to initialize SQL database in application backend

--- dlna/cached/cachedrootfolderreaddirectory.cpp
+++ dlna/cached/cachedrootfolderreaddirectory.cpp
@@ -1,6 +1,34 @@
 #include "cachedrootfolderreaddirectory.h"
 
-CachedRootFolderReadDirectory::CachedRootFolderReadDirectory()
+CachedRootFolderReadDirectory::CachedRootFolderReadDirectory(Logger *log, QDir folder):
+    m_log(log),
+    m_folder(folder)
 {
 
+}
+
+CachedRootFolderReadDirectory::~CachedRootFolderReadDirectory()
+{
+    qDebug() << "Read directory" << m_folder << "finished.";
+}
+
+void CachedRootFolderReadDirectory::run()
+{
+    {
+        // initialize database in current Thread
+        QSqlDatabase database = CREATE_DATABASE("QSQLITE", "MEDIA_DATABASE");
+        database.setDatabaseName("/Users/doudou/workspaceQT/DLNA_server/MEDIA.database");
+
+        if (database.isValid())
+        {
+            DlnaCachedRootFolder root(m_log, "HOST", 80);
+            root.readDirectory(m_folder);
+        }
+        else
+        {
+            qCritical() << "unable to initialize database connection.";
+        }
+    }
+
+    REMOVE_DATABASE("MEDIA_DATABASE");
 }
--- dlna/cached/cachedrootfolderreaddirectory.h
+++ dlna/cached/cachedrootfolderreaddirectory.h
@@ -1,11 +1,23 @@
 #ifndef CACHEDROOTFOLDERREADDIRECTORY_H
 #define CACHEDROOTFOLDERREADDIRECTORY_H
 
+#include <QRunnable>
+
+#include "dlnacachedrootfolder.h"
+#include "logger.h"
 
 class CachedRootFolderReadDirectory : public QRunnable
 {
+
 public:
-    CachedRootFolderReadDirectory();
+    CachedRootFolderReadDirectory(Logger *log, QDir folder);
+    ~CachedRootFolderReadDirectory();
+
+    void run();
+
+private:
+    Logger *m_log;
+    QDir m_folder;
 };
 
 #endif // CACHEDROOTFOLDERREADDIRECTORY_H
