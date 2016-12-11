567784ed4cf3cc9d8a9b32d8637e6ffbf669e168
HIMBERT Guillaume <guillaume.himbert@gmail.com>
2016-12-11 18:19:23 +0100

    improve robustness

--- dlna/dlnayoutubevideo.cpp
+++ dlna/dlnayoutubevideo.cpp
@@ -34,6 +34,7 @@ void DlnaYouTubeVideo::setNetworkAccessManager(QNetworkAccessManager *manager)
 {
     if (manager)
     {
+        mutex.lock();
         m_youtube = new YouTube();
         connect(this, SIGNAL(destroyed()), m_youtube, SLOT(deleteLater()));
 
@@ -46,19 +47,24 @@ void DlnaYouTubeVideo::setNetworkAccessManager(QNetworkAccessManager *manager)
         connect(m_youtube, SIGNAL(gotVideoTitle(QString)), this, SLOT(videoTitle(QString)));
         connect(m_youtube, SIGNAL(gotVideoUrl(QString)), this, SLOT(videoUrl(QString)));
         connect(m_youtube, SIGNAL(videoUrlError(QString)), this, SLOT(videoUrlError(QString)));
+        mutex.unlock();
     }
 }
 
 void DlnaYouTubeVideo::setPlaybackQuality(const QString &quality)
 {
+    mutex.lock();
     if (m_youtube)
         m_youtube->setPlaybackQuality(quality);
     else
         logError("Unable to set playback quality because Youtube is not initialized (call setNetworkAccessManager before).");
+    mutex.unlock();
 }
 
 void DlnaYouTubeVideo::setUrl(const QUrl &url)
 {
+    mutex.lock();
+
     m_videoUrlInProgress = false;
 
     QRegularExpression youtubeUrl("^http.*watch\\?v=(.*)$");
@@ -74,43 +80,66 @@ void DlnaYouTubeVideo::setUrl(const QUrl &url)
     {
         qWarning() << "ERROR, invalid URL" << url;
     }
+
+    mutex.unlock();
 }
 
 void DlnaYouTubeVideo::videoUrlError(const QString &message)
 {
-    m_videoUrlInProgress = false;
+    mutex.lock();
+
     m_error = message;
-    replyWaitCondition.wakeAll();
+
+    if (m_videoUrlInProgress)
+    {
+        m_videoUrlInProgress = false;
+        replyWaitCondition.wakeAll();
+    }
+
+    mutex.unlock();
 }
 
 void DlnaYouTubeVideo::videoTitle(const QString &title)
 {
+    mutex.lock();
     m_title = title;
+    mutex.unlock();
 }
 
 void DlnaYouTubeVideo::videoUrl(const QString &url)
 {
+    mutex.lock();
     m_streamUrl = url;
 
     if (m_analyzeStream)
     {
         ffmpeg.setFilename(url);
-
-        // wait ffmpeg is ready to declare process finished
     }
     else
     {
         // url is found, ffmpeg is not run to probe the media so process finished
-        m_videoUrlInProgress = false;
-        replyWaitCondition.wakeAll();
+        if (m_videoUrlInProgress)
+        {
+            m_videoUrlInProgress = false;
+            replyWaitCondition.wakeAll();
+        }
     }
+
+    mutex.unlock();
 }
 
 void DlnaYouTubeVideo::ffmpegReady()
 {
+    mutex.lock();
+
     // url is found and ffmpeg is ready, process finished
-    m_videoUrlInProgress = false;
-    replyWaitCondition.wakeAll();
+    if (m_videoUrlInProgress)
+    {
+        m_videoUrlInProgress = false;
+        replyWaitCondition.wakeAll();
+    }
+
+    mutex.unlock();
 }
 
 bool DlnaYouTubeVideo::waitUrl(const int &timeout)
