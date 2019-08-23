d4134699ff6e497b0f4fd49242140844855991bd
HIMBERT Guillaume <guillaume.himbert@gmail.com>
2019-08-23 12:05:32 +0200

    minor improvement

--- app/mediaserver.cpp
+++ app/mediaserver.cpp
@@ -13,8 +13,7 @@ MediaServer::MediaServer(const QString &macAddress, const QString& host, int por
 
     initContentDirectory();
 
-    Protocol *dlnaProfiles = new Protocol(":xml profiles/dlna_profiles.xml", m_renderersModel);
-    m_renderersModel->setDlnaProfiles(dlnaProfiles);
+    m_renderersModel->setDlnaProfilesPath(":xml profiles/dlna_profiles.xml");
 }
 
 void MediaServer::initDescription()
